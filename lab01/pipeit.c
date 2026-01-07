c212a319aafd760b55962a5ea7bd828ac67fca95
jasonktov <tov@calpoly.edu>
2026-01-07 15:21:33 -0800

    final commit

--- lab01/pipeit.c
+++ lab01/pipeit.c
@@ -6,17 +6,16 @@
 
 int pipefd[2];
 
-
 int main(){
 	if(pipe(pipefd) == -1){
-		perror("Pipe Failed\n");
+		printf("Pipe Failed\n");
 		return 1;
 	}
 
 	pid_t pid1;
 	pid1 = fork();
 	if(pid1 == -1){
-		perror("Child 1 Fork Failed\n");
+		printf("Child 1 Fork Failed\n");
 		close(pipefd[0]);
 		close(pipefd[1]);
 		exit(1);
@@ -25,22 +24,30 @@ int main(){
 		//Child 1
 		close(pipefd[0]);
 		if(dup2(pipefd[1], STDOUT_FILENO) == -1){
-			exit(1); //dup failed
+			printf("Child 1 dup failed\n");
+			exit(1); 
 		}else{
 			close(pipefd[1]);
 			execlp("ls", "ls", NULL);
 
-			exit(2); //exec failed
+			printf("Child 1 exec failed\n");
+			exit(2); 
 		}
 	}else{
 		//Parent
 		pid_t pid2;
 		pid2 = fork();
 		if(pid2 == -1){
-			perror("Child 2 Fork Failed\n");
+			printf("Child 2 Fork Failed\n");
 			close(pipefd[0]);
 			close(pipefd[1]);
-			wait(NULL);
+
+			int wstatus;
+			waitpid(pid1, &wstatus, 0);
+			if(WIFEXITED(wstatus)){
+				printf("Child 1 exited with exit code %d\n", WEXITSTATUS(wstatus));
+				pid1 = 0;
+			}	
 			exit(1);
 
 		}else if(pid2 == 0){
@@ -48,58 +55,43 @@ int main(){
 			close(pipefd[1]);
 			int outfd = creat("outfile", 0777);
 			if(outfd == -1){
-				exit(3); // file creation failed
+				printf("Child 2 file creation failed\n");
+				exit(3);
 			}else{
 				if(dup2(pipefd[0], STDIN_FILENO) == -1){
+					printf("Child 2 dup failed\n");
 					exit(1);
 				}
 				if(dup2(outfd, STDOUT_FILENO) == -1){
+					printf("Child 2 dup failed\n");
 					exit(1);
 				}
 
 				close(pipefd[0]);
 				execlp("sort", "sort", "-r", NULL);
 
-				exit(2); //exec failed
+				printf("Child 2 exec failed\n");
+				exit(2);
 			}
 
 		}else{
+			//Parent
 			close(pipefd[0]);
 			close(pipefd[1]);
-			//Parent
-
-			int wstatus;
-			int waiting = 2;
 			
-			while(waiting){
-				if(waitpid(pid1, &wstatus, WNOHANG)){
+			int wstatus;
+			while(pid1 | pid2){
+				if(pid1 && waitpid(pid1, &wstatus, WNOHANG)){
 					if(WIFEXITED(wstatus)){
-						switch(WEXITSTATUS(wstatus)){
-							case 1:
-								perror("Child 1 dup failed");
-								break;
-							case 2:
-								perror("Child 1 exec failed");
-								break;
-						}
-					}
-					waiting--;		
+						printf("Child 1 exited with exit code %d\n", WEXITSTATUS(wstatus));
+						pid1 = 0;
+					}	
 				}
-				if(waitpid(pid2, &wstatus, WNOHANG)){
+				if(pid2 && waitpid(pid2, &wstatus, WNOHANG)){
 					if(WIFEXITED(wstatus)){
-						switch(WEXITSTATUS(wstatus)){
-							case 1:
-								perror("Child 2 dup failed");
-								break;
-							case 2:
-								perror("Child 2 exec failed");
-								break;
-							case 3:
-								perror("Child 2 file creation failed");
-								break;
-						}
-					}
-					waiting--;
+						printf("Child 2 exited with exit code %d\n", WEXITSTATUS(wstatus));
+						pid2 = 0;
+					}	
 				}
 			}			
 		}
