4b7d6e272351ed67bea353898d40c4d33087f92c
jasonktov <tov@calpoly.edu>
2026-02-04 15:42:31 -0800

    dine

--- project3/phil.c
+++ project3/phil.c
@@ -2,7 +2,11 @@
 #include <pthread.h>
 #include <semaphore.h>
 
+#include "dawdle.c"
+
+#ifndef NUM_PHILOSOPHERS
 #define NUM_PHILOSOPHERS 8
+#endif
 
 #ifndef TRUE
 #define TRUE 1
@@ -37,6 +41,38 @@ static void initialize_status(p_state state){
     }
 }
 
+static void print_header(){
+    //line 1
+    for(int i = 0; i < NUM_PHILOSOPHERS; i++){
+        printf("|=");
+        for(int f = 0; f < NUM_PHILOSOPHERS; f++){
+            printf("=");
+        }
+        printf("=======");
+    }
+    printf("|\n");
+    
+    //line 2
+    for(int i = 0; i < NUM_PHILOSOPHERS; i++){
+        printf("| ");
+        for(int f = 0; f < NUM_PHILOSOPHERS; f++){
+            printf(" ");
+        }
+        printf("%c      ", 'A'+i);
+    }
+    printf("|\n");
+    
+    //line 3
+    for(int i = 0; i < NUM_PHILOSOPHERS; i++){
+        printf("|=");
+        for(int f = 0; f < NUM_PHILOSOPHERS; f++){
+            printf("=");
+        }
+        printf("=======");
+    }
+    printf("|\n");
+}
+
 static void update_status(int seat, int left_fork, int right_fork, p_state state){
     //updates the global struct that records the status of all philosophers
     //then prints out the status of all philosophers
@@ -48,20 +84,37 @@ static void update_status(int seat, int left_fork, int right_fork, p_state state
     statuses[seat].right_fork = right_fork;
     statuses[seat].state = state;
     
-    printf("%c:", 'A'+seat);
     for(int i = 0; i< NUM_PHILOSOPHERS; i++){
-        p_status cur_status = statuses[i];
+        p_status cur_phil = statuses[i];
         
-        char lf = '-';
-        char rf = '-';
-        if(cur_status.left_fork == TRUE){
-            lf = '0' + i; 
-        }
-        if(cur_status.right_fork == TRUE){
-            rf = '0' + (i+1)% NUM_PHILOSOPHERS;
+        char state_msg[6];
+        if(cur_phil.state == PHIL_EATING){
+            strncpy(state_msg, "Eat  ", 6);
+        }else if(cur_phil.state == PHIL_THINKING){
+            strncpy(state_msg, "Think", 6);
+        }else{
+            strncpy(state_msg, "     ", 6);
         }
         
-        printf("| %c%c%c : %d ", lf, 'A'+i, rf, cur_status.state);
+        int lf_i = -1;
+        int rf_i = -1;
+        if(cur_phil.left_fork == TRUE){
+            lf_i = i;
+        }
+        if(cur_phil.right_fork == TRUE){
+            rf_i = (i+1)%NUM_PHILOSOPHERS;
+        }
+        
+        char forks[NUM_PHILOSOPHERS];
+        for(int f = 0; f < NUM_PHILOSOPHERS; f++){
+            if(f == lf_i || f == rf_i){
+                forks[f] = '0' + f;
+            }else{
+                forks[f] = '-';
+            }
+        }
+            
+        printf("| %s %s ", forks, state_msg);
     }
     printf("|\n");
     sem_post(&printer);
@@ -126,7 +179,7 @@ void* philosopher(void* arg){
             break;
         case PHIL_EATING:
             update_status(seat, TRUE, TRUE, PHIL_EATING);
-            //dawdle();
+            dawdle();
             cycles--;
             if(cycles > 0){
                 next_state = PHIL_THINKING;
@@ -138,7 +191,7 @@ void* philosopher(void* arg){
             break;
         case PHIL_THINKING:
             update_status(seat, FALSE, FALSE, PHIL_THINKING);
-            //dawdle();
+            dawdle();
             next_state = PHIL_EATING;
             state = PHIL_CHANGING;
             break;
@@ -179,12 +232,11 @@ int main(int argc, char** argv){
         if(res != 0){
             printf("main:pthread_create error(%d)\n", res);
             return 1;
-        }else{
-            printf("main:pthread_create good(%c)\n", 'A'+i);
         }
     }
     
     initialize_status(PHIL_CHANGING);
+    print_header();
     
     //post semaphores
     for(int i = 0; i < NUM_PHILOSOPHERS; i++){
@@ -196,6 +248,5 @@ int main(int argc, char** argv){
     for(int i = 0; i < NUM_PHILOSOPHERS; i++){
         pthread_join(childid[i], NULL);
     }
-    printf("main:all philosophers ended\n");
     return 0;
 }
